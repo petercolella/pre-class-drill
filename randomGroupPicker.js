6a798b6d508e1eb36a1ccf6153b9fda8e8e38a01
petercolella <peter.joseph.colella@gmail.com>
2019-05-06 12:52:52 -0400

    refactor of randomGroupPicker

--- randomGroupPicker.js
+++ randomGroupPicker.js
@@ -3,11 +3,13 @@ const groups = ['pink', 'orange', 'red', 'purple', 'blue', 'green'];
 
 function randomGroupPicker(teachArr, groupArr) {
   const tempGroupArr = [...groupArr];
-  const newObj = teachArr.reduce((obj, key) => ({ ...obj, [key]: [] }), {});
-
+  const newObj = {};
   for (let i = 0; i < groupArr.length; i += teachArr.length) {
     for (let key of teachArr) {
       if (tempGroupArr.length > 0) {
+        if (!newObj[key]) {
+          newObj[key] = [];
+        }
         let index = Math.floor(Math.random() * tempGroupArr.length);
         newObj[key].push(tempGroupArr.splice(index, 1).pop());
       }
