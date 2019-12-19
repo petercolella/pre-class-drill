bb648c0ae9d0a71ccad8d7900b345e23c5cf5072
petercolella <peter.joseph.colella@gmail.com>
2019-12-19 15:59:55 -0500

    adds pseudocode for pre-class drills

---
+++ levar.js
@@ -0,0 +1,25 @@
+function writeTime(sec) {
+  // Determine how many hours as an integer.
+  // If the hours integer is a single digit, put a zero in front of it.
+  // Determine how many minutes as an integer, after accounting for the seconds which make up the number of hours.
+  // If the minutes integer is a single digit, put a zero in front of it.
+  // Determine how many seconds as an integer, after accounting for the seconds which make up both the number of hours and of minutes.
+  // If the seconds integer is a single digit, put a zero in front of it.
+  // return hours, minutes, and seconds as one string with a colon separating each one.
+}
+
+function makeChange(cents) {
+  // Initialize the coins object.
+  // store the number of quarters as the dividend of the argument value divided by 25.
+  // store the remainder as the new value we're going to be calculating coin counts off of.
+  // repeat for dimes, nickels and finally pennies.
+  // return the final coins object.
+}
+
+function reverseDigit(num) {
+  // Initialize the new number as an empty string.
+  // Convert the given number (num) to a string using an available method.
+  // Loop through the number as a string from the end to the beginning by starting at the end and decrementing.
+  // At each increment, concatenate the current charcter to the new number string.
+  // Parse the reversed string into an integer and return it.
+}
